6f3920ab24fe73ae555341de39b8c5dfc5f9263d
colt05-test <colt05-test@users.noreply.github.com>
2016-03-06 15:11:23 -0500

    programming in python and i don't even know python XD

---
+++ bridge_variables.py
@@ -0,0 +1,26 @@
+import scratchapi
+USERNAME = 'blah'
+PASSWORD = 'blah'
+SOURCEPROJECT = '000'
+SOURCEVARIABLE = 'a'
+TARGETPROJECT = '000'
+TARGETVARIABLE = 'a'
+  def refreshprogram()
+  s = scratchapi.ScratchUserSession(USERNAME, PASSWORD)
+  var = (s.cloud.get_var(a, str(SOURCEPROJECT)))
+  s.cloud.set_var(TARGETVARIABLE, var, str(TARGETPROJECT))
+  def startprogram()
+  print "Enter the source project ID: ",
+  SOURCEPROJECT = raw_input()
+  print "Enter the target project ID: ",
+  TARGETPROJECT = raw_input()
+  print "Enter the source project variable: ",
+  SOURCEVARIABLE = raw_input()
+  print "Enter the target project variable: ",
+  TARGETVARIABLE = raw_input()
+  while True:
+  print 'sleeping.'
+  time.sleep(10)
+  print 'refreshing.'
+  refreshprogram()
+  
